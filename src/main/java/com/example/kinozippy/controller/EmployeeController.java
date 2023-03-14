bbfa7427c6fe775cc6426374a9c1f605822a5123
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-03-14 12:14:34 +0100

    change to singular endpoint

--- src/main/java/com/example/kinozippy/controller/EmployeeController.java
+++ src/main/java/com/example/kinozippy/controller/EmployeeController.java
@@ -22,27 +22,27 @@ public class EmployeeController {
     }
 
     // handles POST requests to /employees and adds a new Employee entity to the repository.
-    @PostMapping("/employees")
+    @PostMapping("/employee")
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
     // handles GET requests to /employees/{id} and returns the Employee entity with the specified ID.
-    @GetMapping("/employees/{id}")
+    @GetMapping("/employee/{id}")
     public Employee getEmployeeById(@PathVariable(value = "id") int employeeId) {
         return employeeRepository.findById(employeeId)
                 .orElseThrow(null);
     }
 
     // handles GET requests to /employees/search and returns the Employee entity with the specified
-    @GetMapping("/employees/search")
+    @GetMapping("/employee/search")
     public Employee getEmployeeByUsernameAndPassword(@RequestParam(value = "username") String username,
                                                      @RequestParam(value = "password") String password) {
         return employeeRepository.findByUsernameAndPassword(username, password);
     }
 
     // handles PUT requests to /employees/{id} and updates the Employee entity with the specified ID.
-    @PutMapping("/employees/{id}")
+    @PutMapping("/employee/{id}")
     public Employee updateEmployee(@PathVariable(value = "id") int employeeId,
                                    @RequestBody Employee employeeDetails) {
 
@@ -58,7 +58,7 @@ public class EmployeeController {
     }
 
     // handles DELETE requests to /employees/{id} and deletes the Employee entity with the specified ID.
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping("/employee/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable(value = "id") int employeeId) {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElse(null);
