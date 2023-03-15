734fad0150e89e9c3a528e9966ad24e0cfee3343
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-03-15 10:18:29 +0100

    add @CrossOrigin(value = "*") for controllers

--- src/main/java/com/example/kinozippy/controller/TicketController.java
+++ src/main/java/com/example/kinozippy/controller/TicketController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(value = "*")
 public class TicketController {
     private final TicketRepository ticketRepository;
 
