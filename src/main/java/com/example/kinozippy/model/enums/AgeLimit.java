48a6324604d08f0d44357b7ebe405ef7d5eeab69
Marcus <marc303i@stud.kea.dk>
2023-03-15 10:12:59 +0100

    Nu kan AgeLimit.java og Category.java enum give en liste af alle dets enum værdier som en liste

--- src/main/java/com/example/kinozippy/model/enums/AgeLimit.java
+++ src/main/java/com/example/kinozippy/model/enums/AgeLimit.java
@@ -1,28 +1,24 @@
 package com.example.kinozippy.model.enums;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public enum AgeLimit {
-    GENERAL_AUDIENCES(1, 0),              // G – General Audiences
-    PARENTAL_GUIDANCE_SUGGESTED(2, 7),    // PG – Parental Guidance Suggested
-    PARENTS_STRONGLY_CAUTIONED(3, 13),     // PG-13 – Parents Strongly Cautioned
-    RESTRICTED(4, 15),                     // R – Restricted
-    ADULTS_ONLY(5, 17);                     // NC-17 – Adults Only
+    GENERAL_AUDIENCES(0),              // G – General Audiences
+    PARENTAL_GUIDANCE_SUGGESTED(7),    // PG – Parental Guidance Suggested
+    PARENTS_STRONGLY_CAUTIONED(13),     // PG-13 – Parents Strongly Cautioned
+    RESTRICTED(15),                     // R – Restricted
+    ADULTS_ONLY(17);                     // NC-17 – Adults Only
 
-    private final int id;
+
     private final int minimumAge;
-    private static final int amountOfAgeLimits = 5;
     // Constructor
-    AgeLimit (int id, int minimumAge) {
-        this.id = id;
+    AgeLimit (int minimumAge) {
         this.minimumAge = minimumAge;
     }
 
     // Getter
-    public int getId() {
-        return id;
-    }
     public int getMinimumAge() {
         return minimumAge;
     }
@@ -30,34 +26,6 @@ public enum AgeLimit {
 
     // Custom made Metoder
     public static List<AgeLimit> getAllAgeLimits() {
-        List<AgeLimit> ageLimits = new ArrayList<>();
-        for (int i = 1; i <= amountOfAgeLimits; i++) {
-            ageLimits.add(getAgeLimit(i));
-        }
-        return ageLimits;
-    }
-
-    private static AgeLimit getAgeLimit(int id) {
-        switch (id) {
-            case 1 -> {
-                return AgeLimit.GENERAL_AUDIENCES;
-            }
-            case 2 -> {
-                return AgeLimit.PARENTAL_GUIDANCE_SUGGESTED;
-            }
-            case 3 -> {
-                return AgeLimit.PARENTS_STRONGLY_CAUTIONED;
-            }
-            case 4 -> {
-                return AgeLimit.RESTRICTED;
-            }
-            case 5 -> {
-                return AgeLimit.ADULTS_ONLY;
-            }
-            default -> {
-                System.err.println("It was not possible to finde the AgeLimit enum using the ID: " + id);
-                throw new RuntimeException();
-            }
-        }
+        return Arrays.asList(AgeLimit.class.getEnumConstants());
     }
 }
--- src/main/java/com/example/kinozippy/model/enums/Category.java
+++ src/main/java/com/example/kinozippy/model/enums/Category.java
@@ -1,84 +1,28 @@
 package com.example.kinozippy.model.enums;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public enum Category {
-    ACTION(1),
-    ADVENTURE(2),
-    ANIMATION(3),
-    BIOGRAPHY(4),
-    COMEDY(5),
-    CRIME(6),
-    DOCUMENTARY(7),
-    DRAMA(8),
-    FAMILY(9),
-    FANTASY(10),
-    HISTORY(11);
+    ACTION,
+    ADVENTURE,
+    ANIMATION,
+    BIOGRAPHY,
+    COMEDY,
+    CRIME,
+    DOCUMENTARY,
+    DRAMA,
+    FAMILY,
+    FANTASY,
+    HISTORY;
 
-    private final int id;
-    private static final int amountOfCategories = 11;
 
-    // Constructor
-    Category (int id) {
-        this.id = id;
-    }
-
-    // Getter
-    public int getId() {
-        return id;
-    }
 
 
     // Custom made Metoder
     public static List<Category> getAllCategories() {
-        List<Category> categories = new ArrayList<>();
-        for (int i = 1; i <= amountOfCategories; i++) {
-            categories.add(getCategory(i));
-        }
-        return categories;
-    }
-
-    private static Category getCategory(int id) {
-        switch (id) {
-            case 1 -> {
-                return Category.ACTION;
-            }
-            case 2 -> {
-                return Category.ADVENTURE;
-            }
-            case 3 -> {
-                return Category.ANIMATION;
-            }
-            case 4 -> {
-                return Category.BIOGRAPHY;
-            }
-            case 5 -> {
-                return Category.COMEDY;
-            }
-            case 6 -> {
-                return Category.CRIME;
-            }
-            case 7 -> {
-                return Category.DOCUMENTARY;
-            }
-            case 8 -> {
-                return Category.DRAMA;
-            }
-            case 9 -> {
-                return Category.FAMILY;
-            }
-            case 10 -> {
-                return Category.FANTASY;
-            }
-            case 11 -> {
-                return Category.HISTORY;
-            }
-            default -> {
-                System.err.println("It was not possible to finde the Category enum using the ID: " + id);
-                throw new RuntimeException();
-            }
-        }
+        return Arrays.asList(Category.class.getEnumConstants());
     }
 
 
