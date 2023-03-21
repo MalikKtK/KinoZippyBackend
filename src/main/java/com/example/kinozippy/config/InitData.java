d6f273ce50029b5cd777d5540239fd3d3a743b46
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-03-21 14:08:38 +0100

    working model layer

--- src/main/java/com/example/kinozippy/config/InitData.java
+++ src/main/java/com/example/kinozippy/config/InitData.java
@@ -9,9 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Component
 public class InitData implements CommandLineRunner {
